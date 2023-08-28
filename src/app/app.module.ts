65c4727cae7d04cb746f35916c58c9a278e24583
unknown <germanramirezrob@gmail.com>
2023-08-28 12:36:46 -0500

    table list with angular material, sort, fetch added, pagination added

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToolbarComponent,
+  ],
+  exports: [],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
